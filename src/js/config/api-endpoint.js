1e9f3b56c1a500e02df1b7f23e8ee569ad4f6498
yudistiraardi <mail.ardix@gmail.com>
2023-11-30 17:47:21 +0700

    Create API config

---
+++ src/js/config/api-endpoint.js
@@ -0,0 +1,14 @@
+import Config from "./config";
+
+const ApiEndpoint = {
+  REGISTER: `${Config.BASE_URL}/register`,
+  LOGIN: `${Config.BASE_URL}/login`,
+  ADD_NEW: `${Config.BASE_URL}/stories`,
+  GUEST_ADD_NEW: `${Config.BASE_URL}/stories/guest`,
+  DETAIL_STORY: (id) => `${Config.BASE_URL}/stories/${id}`,
+  ALL_STORIES: (page, size, loc) => `
+    ${Config.BASE_URL}/stories?page=${page ? page : ""}&size=${size ? size : ""}&location=${loc ? loc : ""}
+  `,
+}
+
+export default ApiEndpoint;
---
+++ src/js/config/config.js
@@ -0,0 +1,5 @@
+const Config = {
+  BASE_URL: 'https://story-api.dicoding.dev/v1',
+}
+
+export default Config;
